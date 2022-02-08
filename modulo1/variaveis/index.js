a5e129d026bf81c339e27fadd2d582565535202f
Leandro <leandro.ap.leite@gmail.com>
2022-02-07 22:41:48 -0300

    brinquei com as variaveis para fazer uma conta

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,5 @@
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let i = prompt ("Quanto você recebe por mês?")
+let j = prompt ("Quantos dias você trabalha por mês?")
+alert(`Voce recebe ${i/[p*j]} por hora`)
