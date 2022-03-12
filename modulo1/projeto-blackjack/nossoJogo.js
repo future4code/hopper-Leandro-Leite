648ef60836b57df227d7ed438d96a2f6d8f83118
Leandro <leandro.ap.leite@gmail.com>
2022-03-11 23:58:22 -0300

    projeto blackjack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,4 +10,31 @@
  * 
  * 
  */
-    console.log( "Boas vindas ao jogo de Blackjack!")
+console.log("Boas vindas ao jogo de Blackjack!")
+
+if (confirm("Quer iniciar uma nova rodada?")) {
+   const usuario1 = comprarCarta()
+   const usuario2 = comprarCarta()
+   const computador1 = comprarCarta()
+   const computador2 = comprarCarta()
+
+   const resultadoUsuario = usuario1.valor + usuario2.valor
+   const resultadoComputador = computador1.valor + computador2.valor
+
+   console.log(`Usuário - cartas: ${usuario1.texto} ${usuario2.texto} - pontuação: ${resultadoUsuario}`)
+   console.log(`Computador - cartas: ${computador1.texto} ${computador2.texto} - pontuação: ${resultadoComputador}`)
+
+   if (resultadoComputador === resultadoUsuario) {
+      console.log("Empate!")
+
+   } else if (resultadoComputador < resultadoUsuario) {
+      console.log("O usuário ganhou!")
+
+   } else if (resultadoComputador > resultadoUsuario) {
+   }
+      console.log("O computador ganhou!")
+   } else {
+      console.log ('O jogo acabou')
+   }
+
+
